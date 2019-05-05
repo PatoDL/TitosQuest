7a6275c1049a197aaa2d973b758ffd7e753da519
Pato <patodl2701@gmail.com>
2019-05-05 19:53:38 -0300

    added project, turret, and some assets

---
+++ TTQ/Assets/Scripts/BoxSpawner.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BoxSpawner : MonoBehaviour
+{
+    public GameObject boxPF;
+    [SerializeField] private float timer;
+    void Start()
+    {
+        timer = 0f;
+    }
+
+    void Update()
+    {
+        timer += Time.deltaTime;
+
+        if(timer>5f)
+        {
+            SpawnBox();
+            timer = 0f;
+        }
+    }
+
+    void SpawnBox()
+    {
+        GameObject box = Instantiate(boxPF);
+        box.transform.position = new Vector3(Random.Range(0, 450), 70, Random.Range(0, 450));
+    }
+}
